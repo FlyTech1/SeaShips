630823a7019bad8a8c13816a0d90fc4bce20eec1
Mikhail Spasskiy <spasskiy-mikhhail@yandex.ru>
2024-07-08 16:10:36 +0300

    1

--- solutions/Battle_Logic2.py
+++
@@ -1,9 +0,0 @@
-from PySide6.QtWidgets import QMainWindow
-from game_menu import Ui_game_menu
-
-
-class Window_Game_Menu(QMainWindow):
-    def __init__(self):
-        super(Window_Game_Menu, self).__init__()
-        self.ui = Ui_game_menu()
-        self.ui.setupUi(self)
