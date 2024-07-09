fb3d818c1877e001c8ac7cb8325f540d1a76180c
Mikhail Spasskiy <spasskiy-mikhhail@yandex.ru>
2024-07-09 17:23:02 +0300

    1

--- BattleLogic.py
+++ BattleLogic.py
@@ -14,14 +14,16 @@ class Window_main(QMainWindow):
         self.ui.widget_2.hide()
         self.ui.PB_Play.clicked.connect(self.on_PB_Play_clicked)
         self.ui.PB_TG.clicked.connect(self.on_PB_TG_clicked)
+        self.ui.PB_Rules.clicked.connect(self.on_PB_Rules_clicked)
     def on_PB_Play_clicked(self):
         self.ui.widget.hide()
         self.ui.widget_2.show()
         #self.hide()
     def on_PB_TG_clicked(self):
-        print("NEGRI")
-        url=QUrl("https://t.me/bezdariprogaut")
-        QDesktopServices.openUrl(url)
-
+        url1=QUrl("https://t.me/bezdariprogaut")
+        QDesktopServices.openUrl(url1)
+    def on_PB_Rules_clicked(self):
+        url2 = QUrl("https://PORNOHUB/CASINO/MEF.ru")
+        QDesktopServices.openUrl(url2)
 
 
--- main.py
+++ main.py
@@ -5,5 +5,6 @@ import sys
 if __name__ == "__main__":
     app = QApplication(sys.argv)
     w = Window_main()
+    w.setFixedSize(800,650)
     w.show()
     sys.exit(app.exec())
