107f59ae7b601c9e575b2bc78fc63b0211aa69c7
Mikhail Spasskiy <spasskiy-mikhhail@yandex.ru>
2024-07-09 19:16:22 +0300

    1

--- BattleLogic.py
+++ BattleLogic.py
@@ -3,9 +3,8 @@ import sys
 from PySide6.QtWidgets import QApplication, QMainWindow,QLabel
 from Window import Ui_MainWindow
 
-from PySide6.QtGui import QDesktopServices
-from PySide6 import  QtGui, QtCore
-from PySide6.QtCore import QUrl
+from PySide6.QtGui import QDesktopServices, QCursor
+from PySide6.QtCore import QUrl, Qt
 class Window_main(QMainWindow):
     def __init__(self):
         super(Window_main,self).__init__()
@@ -15,6 +14,7 @@ class Window_main(QMainWindow):
         self.ui.PB_Play.clicked.connect(self.on_PB_Play_clicked)
         self.ui.PB_TG.clicked.connect(self.on_PB_TG_clicked)
         self.ui.PB_Rules.clicked.connect(self.on_PB_Rules_clicked)
+        self.dragging=False
     def on_PB_Play_clicked(self):
         self.ui.widget.hide()
         self.ui.widget_2.show()
@@ -25,5 +25,22 @@ class Window_main(QMainWindow):
     def on_PB_Rules_clicked(self):
         url2 = QUrl("https://PORNOHUB/CASINO/MEF.ru")
         QDesktopServices.openUrl(url2)
+
+    def mouseMoveEvent(self, event):
+        print("123")
+        if self.dragging:
+            self.ui.Ship_1.move(event.pos() - self.offset)
+    def mousePressEvent(self, event):
+        if(event.buttons()==Qt.MouseButton.LeftButton and self.ui.Ship_1.geometry().contains(event.pos())):
+            self.dragging = True
+            print("123")
+            self.offset=event.pos()-self.ui.Ship_1.pos()
+            print(event.pos())
+            print(self.offset)
+
+
+
+    def mouseReleaseEvent(self, event):
+        self.dragging = False
 
 
